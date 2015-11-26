96a3d983a9e6756ff65e9647d372a985ad911980
Ivan Garcia <ivanrafael_gescobedo@hotmail.com>
2015-11-25 23:50:40 -0600

    commit

--- start.lua
+++ start.lua
@@ -15,13 +15,11 @@ function scene:create(event)
 	local sceneGroup = self.view
 
 	-- -- Add objects to sceneGrp to be shown in screen.
-	background = display.newImageRect("imgs/black-texture.png", 320, 480)
-	background.anchorX = 0.5
-	background.anchorY = 1
-	background.x = display.contentCenterX
-	background.y = display.contentHeight
-	sceneGroup:insert(background)
+	loadBorders(sceneGroup)
 
+	loadGhosts(sceneGroup)
+
+	charactersAnimation()
 end
 
 function scene:show(event)
@@ -35,6 +33,97 @@ end
 function scene:destroy(event)
 
 end
+
+function charactersAnimation()
+	ghostRedTransitionRight()
+	-- ghostBlueTransitionRight()
+	-- ghostPurpleTransitionRight()
+	-- ghostYellowTransitionRight()
+	-- pacmanTransitionRight()
+end
+
+function ghostRedTransitionRight()
+	-- downTransition = transition.to(titleGroup,{time=400, y=titleGroup.y+20,onComplete=ghostRedTransitionLeft})
+	-- downTransition = transition.to(ghostRed,{time=400, y=ghostRed.y+20,onComplete=ghostRedTransitionLeft})
+	-- ghostRed
+end
+
+function ghostRedTransitionLeft()
+	-- upTransition = transition.to(titleGroup,{time=400, y=titleGroup.y-20, onComplete=ghostRedTransitionRight})
+	-- upTransition = transition.to(ghostRed,{time=400, y=ghostRed.y-20, onComplete=ghostRedTransitionRight})
+end
+
+function loadGhosts(sceneGroup)
+	ghostBlue = display.newImageRect("imgs/ghost-blue.png", 100, 100)
+	ghostBlue.anchorX = 0.5
+	ghostBlue.anchorY = 1
+	ghostBlue.x = display.contentCenterX - 125
+	ghostBlue.y = display.contentHeight - 392
+	sceneGroup:insert(ghostBlue)
+
+	ghostPurple = display.newImageRect("imgs/ghost-purple.png", 100, 100)
+	ghostPurple.anchorX = 0.5
+	ghostPurple.anchorY = 1
+	ghostPurple.x = display.contentCenterX + 155
+	ghostPurple.y = display.contentHeight - 392
+	sceneGroup:insert(ghostPurple)
+
+	ghostRed = display.newImageRect("imgs/ghost-red.png", 100, 100)
+	ghostRed.anchorX = 0.5
+	ghostRed.anchorY = 1
+	ghostRed.x = display.contentCenterX - 125
+	ghostRed.y = display.contentHeight - 53
+	sceneGroup:insert(ghostRed)
+
+	ghostYellow = display.newImageRect("imgs/ghost-yellow.png", 100, 100)
+	ghostYellow.anchorX = 0.5
+	ghostYellow.anchorY = 1
+	ghostYellow.x = display.contentCenterX + 155
+	ghostYellow.y = display.contentHeight - 53
+	sceneGroup:insert(ghostYellow)
+end
+
+function loadBorders(sceneGroup)
+	background = display.newImageRect("imgs/black-texture.png", 320, 480)
+	background.anchorX = 0.5
+	background.anchorY = 1
+	background.x = display.contentCenterX
+	background.y = display.contentHeight
+	sceneGroup:insert(background)
+
+	borderUp = display.newImageRect("imgs/div-border-up.png", 372, 250)
+	borderUp.anchorX = 0.5
+	borderUp.anchorY = 1
+	borderUp.x = display.contentCenterX + 23
+	borderUp.y = display.contentHeight - 239
+	physics.addBody(borderUp, "static", {density=.1, bounce=0.1, friction=.2})
+	sceneGroup:insert(borderUp)
+
+	borderDown = display.newImageRect("imgs/div-border-down.png", 372, 250)
+	borderDown.anchorX = 0.5
+	borderDown.anchorY = 1
+	borderDown.x = display.contentCenterX - 23
+	borderDown.y = display.contentHeight - 95
+	physics.addBody(borderDown, "static", {density=.1, bounce=0.1, friction=.2})
+	sceneGroup:insert(borderDown)
+
+	borderLeft = display.newImageRect("imgs/div-border-left.png", 372, 431)
+	borderLeft.anchorX = 0.5
+	borderLeft.anchorY = 1
+	borderLeft.x = display.contentCenterX + 13
+	borderLeft.y = display.contentHeight - 104
+	physics.addBody(borderLeft, "static", {density=.1, bounce=0.1, friction=.2})
+	sceneGroup:insert(borderLeft)
+
+	borderRight = display.newImageRect("imgs/div-border-right.png", 372, 431)
+	borderRight.anchorX = 0.5
+	borderRight.anchorY = 1
+	borderRight.x = display.contentCenterX - 13
+	borderRight.y = display.contentHeight - 49
+	physics.addBody(borderRight, "static", {density=.1, bounce=0.1, friction=.2})
+	sceneGroup:insert(borderRight)
+end
+
 
 scene:addEventListener( "create", scene )
 scene:addEventListener( "show", scene )
